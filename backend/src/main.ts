e057632010e7fa74e0e16ff78e897e1c94c16c03
6810110406 <6810110406@psu.ac.th>
2025-12-30 14:55:22 +0700

    greenhousanddevices

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors(); 
-  
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
